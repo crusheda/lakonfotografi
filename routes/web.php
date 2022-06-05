b303da73f125d5d75ad8e6b14c4e4907337080da
Yussuf Faisal <thecrusheda@gmail.com>
2022-06-06 02:27:40 +0700

    FIX Template DONE

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () { return view('welcome'); });
-// Route::get('/', function () {
-//     return view('pages.index');
-// });
+// Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () {
+    return view('pages.index');
+});
 
 Auth::routes();
 
